629d9e2e8abef84707ecf70e16df366c23072218
Mirko Zanecchia <mirko.z@improntaadv.com>
2016-12-07 18:49:23 +0100

    Update provider to enable CORS

--- MKMProvider/index.js
+++ MKMProvider/index.js
@@ -3,6 +3,12 @@ const requests = require('./requests.js');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.get('/price', function(req, res) {
     if(!req.query.name) {
         res.status(400).send({message: 'Missing name param'});
